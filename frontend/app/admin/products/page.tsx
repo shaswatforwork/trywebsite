8f3151b8b5be31312d933992d6a8c44af6d18a6b
shaswatforwork <shaswatforwork@gmail.com>
2025-08-27 20:51:21 +0530

    Delete frontend directory

--- frontend/app/admin/products/page.tsx
+++
@@ -1,2 +0,0 @@
-import ProductAdmin from "./product-admin";
-export default function Page(){ return <ProductAdmin/> }
--- frontend/app/products/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProductView from "./product-view";
-
-export default function Page({ params }:any){
-  return <ProductView id={params.id}/>;
-}
